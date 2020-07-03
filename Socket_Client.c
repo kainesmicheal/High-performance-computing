1e53c919fa17223fa39ebf2776628438bff0778b
Micheal <38526063+kainesmicheal@users.noreply.github.com>
2020-07-03 05:50:46 +0530

    Update Socket_Client.c

--- Socket_Client.c
+++ Socket_Client.c
@@ -22,7 +22,7 @@ int main(int argc, char** argv){
     bzero(&server_socket_addr, sizeof(server_socket_addr));
     server_socket_addr.sin_family = AF_INET;
     server_socket_addr.sin_port = htons(SERVER_PORT);
-    server_socket_addr.sin_addr.s_addr = server_addr_nbo;
+    server_socket_addr.sin_addr.s_addr = server_addr_nho;
 
     if(0>connect(socket_fd,(struct sockaddr*)&server_socket_addr,sizeof(server_socket_addr))){
         fprintf(stderr, "client failed to connect to %s:%d!\n",SERVER_ADDR,SERVER_PORT);
